26ca67854627e2585a9c87ec8dc0f3ad9bcbfa1e
samq-ws <sam.quakenbush@whitesourcesoftware.com>
2022-04-27 13:50:20 -0600

    ignore alerts by project

--- prioritize-ignore.sh
+++ prioritize-ignore.sh
@@ -21,6 +21,14 @@ echo "export WS_PROJECTNAME="$WS_PROJECTNAME
 echo "export WS_PROJECTTOKEN="$WS_PROJECTTOKEN
 echo "export WS_URL="$WS_URL
 
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
 
 
 ### getProjectSecurityAlertsbyVulnerabilityReport - finds Green Shields
@@ -43,11 +51,11 @@ greenshieldlist=$(cat greenshields.txt)
 ### Get CVE by GREEN Shield
 for GREENSHIELDVULN in $greenshieldlist
 do
-echo "GREENSHIELDVULN:"$GREENSHIELDVULN
+echo -e "${grn}GREENSHIELDVULN: $GREENSHIELDVULN${end}"
 
 IGNORES=$(jq -r --arg GREENSHIELDVULN $GREENSHIELDVULN '.alerts[] | select(.vulnerability.name==$GREENSHIELDVULN)|.alertUuid' alerts.json)
-echo "Ignoring the following alertUuids"$IGNORES
+echo "${yel}Ignoring the following alertUuids $IGNORES${end}"
 
-curl --request POST $WS_URL'/api/v1.3' -H 'Content-Type: application/json'  -d '{ "requestType" : "ignoreAlerts", "userKey" : "'$WS_USERKEY'", "orgToken" : "'$WS_APIKEY'", "alertUuids" : "'$IGNORES'", "comments" : "green shield vulnerabilities are not reachable or exploitable and have been ignored"}'
+curl --request POST $WS_URL'/api/v1.3' -H 'Content-Type: application/json'  -d '{ "requestType" : "ignoreAlerts", "userKey" : "'$WS_USERKEY'", "projectToken" : "'$REPOTOKEN'", "alertUuids" : "'$IGNORES'", "comments" : "green shield vulnerabilities are not reachable or exploitable and have been ignored"}'
 
 done
